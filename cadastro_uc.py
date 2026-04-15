f7fbdb4eb49b5c7cdfecbd442090deb0d5e26ab2
Gualter-leonardo <gualterleonardo216@gmail.com>
2026-04-15 19:46:11 -0300

    Update cadastro_uc.py

---
+++ cadastro_uc.py
@@ -0,0 +1,44 @@
+import sys
+import mysql.connector
+from PyQt5 import uic, QtWidgets
+import conexao 
+
+
+def salvar_uc():
+    
+    
+    horas_uc = tela.txt_horasucs.text()
+    posicao = tela.txt_posicao.text()
+    uc= tela.txt_uc.text()
+
+ # Cria o cursor que executa comandos SQL
+    cursor = conexao.cursor()
+    # Comando SQL de inserção
+    comando = "INSERT INTO grade ( horas_uc, posicao, uc) VALUES (%s,%s,%s)"\
+
+    # Valores que serão inseridos
+    dados = ( horas_uc, posicao ,uc)
+    # Executa o comando
+    cursor.execute(comando, dados)
+    # Confirma a alteração no banco
+    conexao.commit()
+    # Mostra mensagem para o usúario
+    QtWidgets.QMessageBox.information(tela, "Sucesso","Grade cadastrada com sucesso")
+    # Limpa os campos da tela
+    
+    tela.txt_horasucs.setText("")
+    tela.txt_posicao.setText("")
+    tela.txt_uc.setText("")
+
+# Inicialização da aplicação
+app = QtWidgets.QApplication([])
+# Carrega a interface criada no QtDesigner
+tela = uic.loadUi("tela/cadastrarcurso.ui")
+# Conecta o botão salvar com a função salvar_aluno
+tela.btn_cadastrar_uc.clicked.connect(salvar_uc)
+# Mostra a tela
+tela.show()
+# Mantém o programa em execução
+app.exec()
+     
+    
