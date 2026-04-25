f851bf61a111b7a3a58746208df6b81d0fefedcf
yanc7747-collab <yanc7747@gmail.com>
2026-04-24 21:08:08 -0300

    01

--- login.py
+++ login.py
@@ -8,29 +8,29 @@ def verificar_login():
     usuario = tela.txt_usuario.text()
     senha = tela.txt_senha.text()
     
-    conexao = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="test"
-    )
-    cursor = conexao.cursor()
+    QtWidgets.QMessageBox.information(tela, "Login", "Login realizado com sucesso")
+    # conexao = mysql.connector.connect(
+    #     host="localhost",
+    #     user="root",
+    #     password="",
+    #     database="test"
+    # )
+    # cursor = conexao.cursor()
     
-    comando = "SELECT * FROM informacao WHERE usuario=%s AND senha=%s"
-    dados = (usuario, senha)
-    cursor.execute(comando, dados)
-    resultado = cursor.fetchone()
-    if resultado:
+    # comando = "SELECT * FROM informacao WHERE usuario=%s AND senha=%s"
+    # dados = (usuario, senha)
+    # cursor.execute(comando, dados)
+    # resultado = cursor.fetchone()
+    # if resultado:
 
-        QtWidgets.QMessageBox.information(tela, "Login", "Login realizado com sucesso")
+        
+    # else:
 
-    else:
-
-        QtWidgets.QMessageBox.warning(tela, "Erro", "Usuario ou senha inválidos")
+    #     QtWidgets.QMessageBox.warning(tela, "Erro", "Usuario ou senha inválidos")
 
 app = QtWidgets.QApplication([])
-tela = uic.loadUi("tela/login.ui")
-tela.btn_login.clicked.connect(verificar_login)
+tela = uic.loadUi("tela/cadastrarcurso.ui")
+# tela.btn_login.clicked.connect(verificar_login)
 tela.show()
 app.exec()
         
