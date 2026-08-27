045da7d218a071a6658bad019b80cd905da9fd5c
Gualter-leonardo <gualterleonardo216@gmail.com>
2026-08-27 19:40:58 -0300

    01

--- run.bat
+++
@@ -1,3 +0,0 @@
-@echo on
-"C:\Users\1027117\AppData\Local\Programs\Python\Python314\python.exe" main.py
-pause
--- run.ps1
+++
@@ -1,1 +0,0 @@
-&'C:\Users\1027117\AppData\Local\Programs\Python\Python314\python.exe' main.py
